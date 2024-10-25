8b7b59d1d86dff4c0bddf4f09f08cb49e5c1175b
jamesyang <jamesyang@gmail.com>
2024-10-25 16:35:09 +0800

    add debug config

--- src/main.c
+++ src/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
-    i(argc > 1){
-        char *config = argv[1];
+    printf("test c demo code....");
+    if(argc > 1){
+        printf("the config file path is %s\n", argv[1]);
     }
-
-    printf("test c demo code....")
     return 0;
 }
